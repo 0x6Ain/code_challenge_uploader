28b8f9be56f17f741d8124edc5b9247587de128a
jinyoung <jinpods@gmail.com>
2025-03-13 22:20:23 +0900

    feat: add github authentication flow

---
+++ scripts/authorize.js
@@ -0,0 +1,104 @@
+/*
+    (needs patch)
+    IMPLEMENTATION OF AUTHENTICATION ROUTE AFTER REDIRECT FROM GITHUB.
+*/
+
+const localAuth = {
+  /**
+   * Initialize
+   */
+  init() {
+    this.KEY = "code_challenge_uploader_token";
+    this.ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+    this.AUTHORIZATION_URL = "https://github.com/login/oauth/authorize";
+    this.CLIENT_ID = "Ov23liyQV5yVqSKGky0S";
+    this.REDIRECT_URL = "https://github.com/";
+    this.SCOPES = ["repo"];
+  },
+
+  /**
+   * Parses Access Code
+   *
+   * @param url The url containing the access code.
+   */
+  parseAccessCode(url) {
+    if (url.match(/\?error=(.+)/)) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var tab = tabs[0];
+        chrome.tabs.remove(tab.id, function () {});
+      });
+    } else {
+      // eslint-disable-next-line
+      this.requestToken(url.match(/\?code=([\w\/\-]+)/)[1]);
+    }
+  },
+
+  /**
+   * Request Token
+   *
+   * @param code The access code returned by provider.
+   */
+  requestToken(code) {
+    const that = this;
+    const data = new FormData();
+    data.append("client_id", this.CLIENT_ID);
+    data.append("code", code);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          that.finish(xhr.responseText.match(/access_token=([^&]*)/)[1]);
+        } else {
+          chrome.runtime.sendMessage({
+            closeWebPage: true,
+            isSuccess: false,
+          });
+        }
+      }
+    });
+    xhr.open("POST", this.ACCESS_TOKEN_URL, true);
+    xhr.send(data);
+  },
+
+  /**
+   * Finish
+   *
+   * @param token The OAuth2 token given to the application from the provider.
+   */
+  finish(token) {
+    /* Get username */
+    // To validate user, load user object from GitHub.
+    const AUTHENTICATION_URL = "https://api.github.com/user";
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const username = JSON.parse(xhr.responseText).login;
+          chrome.runtime.sendMessage({
+            closeWebPage: true,
+            isSuccess: true,
+            token,
+            username,
+            KEY: this.KEY,
+          });
+        }
+      }
+    });
+    xhr.open("GET", AUTHENTICATION_URL, true);
+    xhr.setRequestHeader("Authorization", `token ${token}`);
+    xhr.send();
+  },
+};
+
+localAuth.init(); // load params.
+const link = window.location.href;
+/* Check for open pipe */
+if (window.location.host === "github.com") {
+  chrome.storage.local.get("pipe_code_challenge_uploader", (data) => {
+    if (data && data.pipe_code_challenge_uploader) {
+      localAuth.parseAccessCode(link);
+    }
+  });
+}
---
+++ scripts/background.js
@@ -0,0 +1,49 @@
+function handleMessage(request, sender, sendResponse) {
+  console.log(request);
+  if (request.action === "customCommitMessageUpdated") {
+    chrome.storage.local.set({ custom_commit_message: request.message });
+  }
+
+  if (request && request.closeWebPage === true && request.isSuccess === true) {
+    console.log(request);
+    /* Set username */
+    chrome.storage.local.set({
+      code_challenge_uploader_username: request.username,
+    });
+
+    /* Set token */
+    chrome.storage.local.set({ code_challenge_uploader_token: request.token });
+
+    /* Close pipe */
+    chrome.storage.local.set({ pipe_code_challenge_uploader: false }, () => {
+      console.log("Closed pipe.");
+    });
+
+    chrome.tabs.query(
+      { active: true, lastFocusedWindow: true },
+      function (tabs) {
+        var tab = tabs[0];
+        chrome.tabs.remove(tab.id);
+      }
+    );
+
+    /* Go to onboarding for UX */
+    const urlOnboarding = chrome.runtime.getURL("welcome.html");
+    chrome.tabs.create({ url: urlOnboarding, active: true }); // creates new tab
+  } else if (
+    request &&
+    request.closeWebPage === true &&
+    request.isSuccess === false
+  ) {
+    alert("Something went wrong while trying to authenticate your profile!");
+    chrome.tabs.query(
+      { active: true, lastFocusedWindow: true },
+      function (tabs) {
+        var tab = tabs[0];
+        chrome.tabs.remove(tab.id);
+      }
+    );
+  }
+}
+
+chrome.runtime.onMessage.addListener(handleMessage);
